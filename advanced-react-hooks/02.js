3d501588e1920ef8cfd83f3a4084efd3de8d1c02
github username <github email address>
2020-07-18 13:37:09 -0500

    custom hooks and useContext

---
+++ advanced-react-hooks/02.js
@@ -0,0 +1,129 @@
+// useCallback: custom hooks
+// http://localhost:3000/isolated/exercise/02.js
+
+import React from 'react'
+import {
+  fetchPokemon,
+  PokemonForm,
+  PokemonDataView,
+  PokemonInfoFallback,
+  PokemonErrorBoundary,
+} from '../pokemon'
+
+// 🐨 this is going to be our generic asyncReducer
+function asyncReducer(state, action) {
+  switch (action.type) {
+    case 'pending': {
+      // 🐨 replace "pokemon" with "data"
+      return {status: 'pending', data: null, error: null}
+    }
+    case 'resolved': {
+      // 🐨 replace "pokemon" with "data" (in the action too!)
+      return {status: 'resolved', data: action.data, error: null}
+    }
+    case 'rejected': {
+      // 🐨 replace "pokemon" with "data"
+      return {status: 'rejected', data: null, error: action.error}
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function useAsync(asyncCallback, initialState, dependencies) {
+  const [state, dispatch] = React.useReducer(asyncReducer, {
+    status: 'idle',
+    // 🐨 this'll need to be "data" instead of "pokemon"
+    data: null,
+    error: null,
+    ...initialState,
+  })
+  React.useEffect(() => {
+    // 💰 this first early-exit bit is a little tricky, so let me give you a hint:
+    const promise = asyncCallback()
+    if (!promise) {
+      return
+    }
+    // then you can dispatch and handle the promise etc...
+    dispatch({type: 'pending'})
+    promise.then(
+      data => {
+        dispatch({type: 'resolved', data})
+      },
+      error => {
+        dispatch({type: 'rejected', error})
+      },
+    )
+    // 🐨 you'll accept dependencies as an array and pass that here.
+    // 🐨 because of limitations with ESLint, you'll need to ignore
+    // the react-hooks/exhaustive-deps rule. We'll fix this in an extra credit.
+  }, dependencies)
+
+  return state
+}
+
+function PokemonInfo({pokemonName}) {
+  // 🐨 move both the useReducer and useEffect hooks to a custom hook called useAsync
+  // here's how you use it:
+  const state = useAsync(
+    //this is our asyncCallback. If there is no name, there is no fetch.
+    // so there will be no promise in our useEffect in useAsync hook.
+    () => {
+      if (!pokemonName) {
+        return
+      }
+      return fetchPokemon(pokemonName)
+    },
+    //this is the initialState
+    {status: pokemonName ? 'pending' : 'idle'},
+    //this is the dependencies array for our useAsync hook
+    [pokemonName],
+  )
+
+  // 🐨 so you're job is to create a useAsync function that makes this work.
+  console.log(state)
+  // 🐨 this'll change from "pokemon" to "data"
+  const {data: pokemon, status, error} = state
+
+  if (status === 'idle' || !pokemonName) {
+    return 'Submit a pokemon'
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (status === 'rejected') {
+    return (
+      <div>
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (status === 'resolved') {
+    return <PokemonDataView pokemon={pokemon} />
+  }
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  function handleReset() {
+    setPokemonName('')
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonErrorBoundary onReset={handleReset} resetKeys={[pokemonName]}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default App
