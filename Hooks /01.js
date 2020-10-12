8997809d30c60873d1ccc12ab085d208ccd3914c
github username <github email address>
2020-10-12 11:28:45 -0500

    useState: greeting

--- Hooks /01.js
+++ Hooks /01.js
@@ -1,29 +1,30 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import React, {useState} from 'react'
+import React from "react";
 
-function Greeting({initialName}) {
+function Greeting({ initialName }) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = useState(initialName)
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
-    setName(event.target.value)
+    // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} value={name} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting initialName={'Adrian'} />
+  return <Greeting initialName="Adrian" />;
 }
 
-export default App
+export default App;
