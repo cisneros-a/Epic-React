82719d7cec2af76066bef86cecce908a6bddbdba
github username <github email address>
2020-10-15 13:14:16 -0500

    useRef and useEffect hooks

--- Hooks /05.js
+++ Hooks /05.js
@@ -1,29 +1,47 @@
 // useRef and useEffect: DOM interaction
 // http://localhost:3000/isolated/exercise/05.js
 
-import React from 'react'
+import React from "react";
+import reactDomTestUtilsProductionMin from "react-dom/cjs/react-dom-test-utils.production.min";
 // eslint-disable-next-line no-unused-vars
-import VanillaTilt from 'vanilla-tilt'
+import VanillaTilt from "vanilla-tilt";
 
-function Tilt({children}) {
+//This exercise was to demonstrate that we don't actually have access to the dom nodes in our
+//  render method. JSX is just syntactic sugar for "React.createElement()". And in order to get
+//  access to the DOM nodes are created, we use the useRef() hook. Then we can access the nodes
+//  component has been put on the DOM (mounted) with a useEffect hook by referencing the useRef,
+//  which we have done here.
+//We also make sure to take advantage of the cleanup in useEffect to remove the eventhandler (vanillaTilt)
+//  on the tiltNode before it is unmounted so we don't have uneeded event handlers in the DOM.
+
+function Tilt({ children }) {
   // 🐨 create a ref here with React.useRef()
-  const tiltRef = React.useRef()
-  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
-  // div look fancy.
+
+  const tiltRef = React.useRef();
+
   React.useEffect(() => {
-    const tiltNode = tiltRef.current
+    const tiltNode = tiltRef.current;
     VanillaTilt.init(tiltNode, {
       max: 25,
       speed: 400,
       glare: true,
-      'max-glare': 0.5,
-    })
+      "max-glare": 0.5,
+    });
     return () => {
-      return () => tiltNode.vanillaTilt.destroy()
-    }
-  }, [])
-  // 💰 like this:
+      tiltNode.vanillaTilt.destroy();
+    };
+  }, []);
 
+  // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
+  // div look fancy.
+  // 💰 like this:
+  // const tiltNode = tiltRef.current
+  // VanillaTilt.init(tiltNode, {
+  //   max: 25,
+  //   speed: 400,
+  //   glare: true,
+  //   'max-glare': 0.5,
+  // })
   //
   // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
   // object to your DOM node to cleanup:
@@ -38,7 +56,7 @@ function Tilt({children}) {
     <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -46,7 +64,7 @@ function App() {
     <Tilt>
       <div className="totally-centered">vanilla-tilt.js</div>
     </Tilt>
-  )
+  );
 }
 
-export default App
+export default App;
